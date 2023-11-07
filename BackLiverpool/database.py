356b702bfa3c908d3e15f97491b50c44e6dec8d2
Harumi Cristal <84361406+HarumiManz@users.noreply.github.com>
2023-11-07 12:04:59 -0600

    back

---
+++ BackLiverpool/database.py
@@ -0,0 +1,31 @@
+
+import csv
+from pymongo import MongoClient
+
+
+# Class for handling data upload to the database
+class Reader:
+    # Constructor that user the Mongo Connection string and target database
+    def __init__(self, uri, db_name):
+        self.client = MongoClient(uri)
+        self.db = self.client[db_name]
+        self.knowledge = []
+
+    def read_row(self, row: dict):
+        self.knowledge.append(row)
+
+    def push_to_db(self, collectionName):
+        collection = self.db[collectionName]
+        result = collection.insert_many(self.knowledge)
+        inserted_ids = result.inserted_ids
+
+        print(f"{len(self.knowledge)} rows inserted in NO SQL DB")
+        
+    def get_docs(self, collectionName):
+        collection = self.db[collectionName]
+        documents = collection.find({})
+        res = []
+        for document in documents:
+            document["_id"] = str(document["_id"])
+            res.append(document)
+        return res
