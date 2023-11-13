8b71fa153997dbf31dd435ca9334247340858310
Harumi Cristal <84361406+HarumiManz@users.noreply.github.com>
2023-11-13 16:11:24 -0600

    cambio pagina de inicio

--- liverpool-app/components/Inicio/Datos.tsx
+++ liverpool-app/components/Inicio/Datos.tsx
@@ -77,6 +77,68 @@ export default function Datos() {
             </figcaption>
           </figure>
         </div>
+        <div className="grid mb-8 lg:mb-12 lg:grid-cols-3">
+          <figure className="flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-b border-gray-200 md:p-12 lg:border-r dark:bg-gray-800 dark:border-gray-700">
+            <blockquote className="mx-auto mb-8 max-w-2xl text-gray-500 dark:text-gray-400">
+              <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                LIVERPOOL
+              </h3>
+              <p className="my-4">
+                El 70% de la fuerza laboral dentro de Liverpool para cubrir
+                todas sus posiciones.
+              </p>
+            </blockquote>
+            <figcaption className="flex justify-center items-center space-x-3">
+              <Image
+                src="/images/casa1.jpeg"
+                alt="analitica "
+                width={300}
+                height={300}
+                className="px-10"
+              />
+            </figcaption>
+          </figure>
+          <figure className="flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-b border-gray-200 md:p-12 lg:border-r dark:bg-gray-800 dark:border-gray-700">
+            <blockquote className="mx-auto mb-8 max-w-2xl text-gray-500 dark:text-gray-400">
+              <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                CEDIS
+              </h3>
+              <p className="my-4">
+                3 veces toda la fuerza laboral de CeDis para cubrir todas sus
+                posiciones.
+              </p>
+            </blockquote>
+            <figcaption className="flex justify-center items-center space-x-3">
+              <Image
+                src="/images/casa2.jpeg"
+                alt="analitica "
+                width={300}
+                height={300}
+                className="px-10 "
+              />
+            </figcaption>
+          </figure>
+          <figure className="flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-b border-gray-200 md:p-12 lg:border-r dark:bg-gray-800 dark:border-gray-700">
+            <blockquote className="mx-auto mb-8 max-w-2xl text-gray-500 dark:text-gray-400">
+              <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                SUBURBIA
+              </h3>
+              <p className="my-4">
+                1.5 veces toda la fuerza laboral de Suburbia para cubrir todas
+                sus posiciones.
+              </p>
+            </blockquote>
+            <figcaption className="flex justify-center items-center space-x-3">
+              <Image
+                src="/images/casa3.jpeg"
+                alt="analitica "
+                width={300}
+                height={300}
+                className="px-10 "
+              />
+            </figcaption>
+          </figure>
+        </div>
       </section>
     </>
   );
