b5f5848a7bba507b9af013ef348559c45ce16b2d
annrubio <150752059+annrubio@users.noreply.github.com>
2023-11-13 18:40:54 -0600

    Ann
    de p a P
    acento a Pruébalo

--- liverpool-app/components/Inicio/Bienvenido.tsx
+++ liverpool-app/components/Inicio/Bienvenido.tsx
@@ -7,7 +7,7 @@ export default function Bienvenido() {
         <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div className="mr-auto place-self-center lg:col-span-7 ">
             <h1 className="max-w-2xl mb-4 text-5xl font-bold tracking-tight sm:text-4xl leading-none md:text-5xl xl:text-6xl text-pink-900">
-              ¡Bienvenido a nuestro Portal de predicción
+              ¡Bienvenido a nuestro Portal de Predicción!
             </h1>
             <p className="max-w-2xl mb-6 font-light text-gray-800 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
               "Enfocados en preservar el talento, te invitamos a descubrir cómo
@@ -18,7 +18,7 @@ export default function Bienvenido() {
               href="#inicio"
               className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 font-medium rounded-lg text-m px-9 py-3 text-center mr-2 mb-2"
             >
-              Pruebalo
+              Pruébalo
             </a>
           </div>
           <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
