60e0d1472ecbefdca7cf49418e4d7c6878bef132
Harumi Cristal <84361406+HarumiManz@users.noreply.github.com>
2023-11-16 14:28:57 -0600

    back
    se cambio el back completo

--- BackLiverpool/server.py
+++ BackLiverpool/server.py
@@ -1,110 +1,146 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_file
 from flask_cors import CORS
 from uuid import uuid4
-import csv
 from database import Reader
 import os
 import joblib
-from joblib import load 
-from pymongo import MongoClient
+from pandas import read_csv, read_excel
 
-#pip install catboost 
-#pip install joblib 
-q
+# pip install openpyxl
+
+MONGO_URL = "mongodb://localhost:27017"
+
+MOST_HEADERS = [
+    "edad",
+    "genero",
+    "antiguedad",
+    "cambios de puesto",
+    "area",
+    "tienda",
+    "ubicacion",
+    "generacion",
+]
+
 app = Flask("Back Liverpool")
 CORS(app)
-loaded_model = joblib.load('modelo_Catboost.pkl') 
+
+model = joblib.load("./model.pkl")
+
+most_recent_file = ""
+
+
+def verify_headers(h):
+    for i in MOST_HEADERS:
+        if i not in h:
+            return False
+    return True
 
 
 @app.route("/upload", methods=["POST"])
 def upload():
+    global most_recent_file
     try:
         file = request.files["file"]
     except:
         return jsonify({"message": "Error, no file received", "error": True}), 400
 
+    file_extension = file.filename.rsplit(".", 1)[1].lower()
     file_name = uuid4()
-    file.save(f"./temp/{file_name}.csv")
-    with open(f"./temp/{file_name}.csv", "r") as f:
-        lines = f.readlines()
-        headers = lines[0][:-1].split(",")
-        headers = [word.lower() for word in headers]
-        lines[0] = ",".join(headers) + "\n"
+    temp_file_path = f"./temp/{file_name}.{file_extension}"
+    file.save(temp_file_path)
 
-    with open(f"./temp/{file_name}.csv", "w") as f:
-        f.writelines(lines)
-        
-    try :
-        
-        temp_reader = Reader("mongodb://localhost:27017/", "LiverpoolTestBack")
-        with open(f'./temp/{file_name}.csv', "r") as f:
-            dict_reader = csv.DictReader(f)
-            line = 1
-            for row in dict_reader:
-                temp_reader.read_row(row)
-                line += 1
-        temp_reader.push_to_db(str(file_name))
-        os.remove(f'./temp/{file_name}.csv')
-        del temp_reader
-        
+    if file_extension == "csv":
+        df = read_csv(temp_file_path)
+    elif file_extension in ["xls", "xlsx"]:
+        df = read_excel(temp_file_path)
+    else:
+        return jsonify({"message": "Unsupported file format", "error": True}), 400
+
+    df.columns = [header.lower() for header in df.columns]
+    headers = df.columns.tolist()
+    lines = len(df)
+
+    if not verify_headers(headers):
         return (
-            jsonify(
-                {
-                    "message": "archivo cargado",
-                    "lines": len(lines) - 1,
-                    "attributes": headers,
-                    "id": file_name,
-                    "error": False,
-                }
-            ),
-            200,
+            jsonify({"message": "Missing model necessary header", "error": True}),
+            400,
         )
-        
-         
-        
-    except Exception as err :
+
+    output_file_path = f"./temp/{file_name}.csv"
+    df.to_csv(output_file_path, index=False)
+
+    try:
+        new_data = read_csv(f"./temp/{file_name}.csv")
+        predictions = model.predict(new_data)
+        new_data["Estatus"] = predictions
+        new_data.to_excel(f"./temp/{file_name}.xlsx", index=False, engine="openpyxl")
+
+    except Exception as err:
         print(err)
         return (
             jsonify(
-                {
-                    "message": "error al cargar archivo",
-                    "lines": 0,
-                    "attributes": None,
-                    "id": file_name,
-                    "error": True,
-                }
+                {"message": "an error ocurred when predicitng the file", "error": True}
             ),
             500,
         )
-        
-    
+
+    try:
+        temp_reader = Reader(
+            MONGO_URL,
+            "LiverpoolTestBack",
+        )
+        temp_reader.drop_collection("Archivo")
+
+        df = read_excel(f"./temp/{file_name}.xlsx", engine="openpyxl")
+
+        for _, row in df.iterrows():
+            row_dict = row.to_dict()
+            temp_reader.read_row(row_dict)
+
+        temp_reader.push_to_db("Archivo")
+        del temp_reader
+
+        most_recent_file = file_name
+
+        return (
+            jsonify({"message": "success!", "fileId": file_name, "lines": lines}),
+            200,
+        )
+
+    except Exception as err:
+        return (
+            jsonify(
+                {"message": "an error ocurred when upoading to mongo", "error": True}
+            ),
+            500,
+        )
+
+
+@app.route("/download", methods=["POST"])
+def download_file():
+    fileId = request.form.get("fileId")
+    if not fileId:
+        return jsonify({"message": "no file id in request", "error": True}), 400
+    file_path = f"./temp/{fileId}.xlsx"
+    print(file_path)
+    if os.path.exists(file_path):
+        return send_file(file_path, as_attachment=True)
+    return jsonify({"message": "file dosent exists", "error": True}), 400
+
+
 @app.route("/getAllDocuments")
 def getAllDocuments():
     try:
-        temp_reader = Reader("mongodb://localhost:27017/", "LiverpoolTestBack")
+        temp_reader = Reader(
+            MONGO_URL,
+            "LiverpoolTestBack",
+        )
         data = temp_reader.get_docs("Archivo")
-        return jsonify({"message" : "success", "data" : data})
-    except Exception as err:
-        print(err)
-        return jsonify({"message" : "error"}), 500
-    
-@app.route("/getLatestDocument")
-def getLatestDocument():
-    try:
-        # Establecer conexión con MongoDB
-        client = MongoClient("mongodb://localhost:27017/")
-        db = client["LiverpoolTestBack"]
-        collection = db["Archivo"]
-
-        # Ordenar los documentos por fecha en orden descendente y obtener el primero (el último ingresado)
-        latest_document = collection.find_one({}, sort=[("fecha", -1)])  # Reemplaza "fecha" con el nombre de tu campo de fecha
-
-        return jsonify({"message": "success", "data": latest_document})
-    
+        return jsonify({"message": "success", "data": data})
     except Exception as err:
         print(err)
         return jsonify({"message": "error"}), 500
 
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", debug=True)
+    app.run("0.0.0.0", debug=True, port=8082)
--- liverpool-app/app/api/chat/route.ts
+++ liverpool-app/app/api/chat/route.ts
@@ -1,27 +1,27 @@
-import { NextResponse } from "next/server";
-
 import OpenAI from "openai";
 import { OpenAIStream, StreamingTextResponse } from "ai";
 
-//conectas con la api de open AI 
 const openai = new OpenAI({
   apiKey: "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",
 });
 
-
-//indica que el endpoint va a correr en conjunto con el front 
 export const runtime = "edge";
 
-//end point que manda peticion a openAi 
+const initialMessage = {
+  role: "system",
+  content:
+    "Eres Lia, un asistente virtual especializado en ayudar a dar planes para la rotacion de empleados en la compañia Liverpool, empresa Mexicana. Eres cordial y divertida.",
+};
+
 export async function POST(request: Request) {
   const { messages } = await request.json();
+  const messagesForAPI = [initialMessage, ...messages];
   const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo", //modelo barato, no mover 
-    stream: true, //pasa el texto por palabras 
-    messages,
+    model: "gpt-3.5-turbo",
+    stream: true,
+    messages: messagesForAPI,
   });
 
-  
   const stream = OpenAIStream(response);
   return new StreamingTextResponse(stream);
 }
