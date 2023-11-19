5045ab2d9a4e716a2bd31e4a2a44d39aa3ce429f
Harumi Cristal <84361406+HarumiManz@users.noreply.github.com>
2023-11-18 20:22:23 -0600

    actualizacion back end

--- liverpool-app/components/Herramienta/archivo.tsx
+++ liverpool-app/components/Herramienta/archivo.tsx
@@ -1,34 +1,64 @@
+//npm install --save @lottiefiles/react-lottie-player
+
 "use client";
 import React, { useState } from "react";
-import Image from "next/image";
+import Lottie from "lottie-react";
+import cat from "@/assets/gatito.json";
 
 export default function Archivo() {
   const [file, setFile] = useState<File | null>(null);
   const [fileResponse, setfileResponse] = useState<any>(null);
 
-  const [data, setData] = useState(null);
+  const [fileUploading, setfileUploading] = useState(false);
 
   const handleSubmitFile = async () => {
     if (file) {
       try {
         const form = new FormData();
         form.append("file", file);
+        setfileUploading(true);
         //cambiar url siempre que se corra el server.py
-        const response = await fetch("http://10.48.99.20:8082/upload", {
+        const response = await fetch("http://192.168.68.106:8082/upload", {
           method: "POST",
           body: form,
         });
         const data = await response.json();
+        setfileUploading(false);
         if (!data.error) {
           setfileResponse(data);
           console.log("Resultado : ", data);
         } else {
+          console.log("Resultado : ", data);
           console.log("error subiendo archivo");
         }
       } catch (err) {
         console.log("error subiendo archivo");
       }
     }
+  };
+
+  const downloadFile = async () => {
+    //cambiar url siempre que se corra el server.py 
+    const response = await fetch("http://192.168.68.106:8082/download", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({ fileId: fileResponse.fileId }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Something went wrong");
+    }
+
+    const blob = await response.blob();
+    const downloadUrl = window.URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = downloadUrl;
+    link.setAttribute("download", "file.xlsx"); // Set the file name
+    document.body.appendChild(link);
+    link.click();
+    link.parentNode?.removeChild(link);
   };
 
   return (
@@ -47,29 +77,37 @@ export default function Archivo() {
             <p className="text-xl font-light text-gray-800 pt-3">
               Solo archivos .xlsx con menos de 1 Gb
             </p>
-            <div className="rounded-md border-dashed border-pink-500 border-2 p-2 m-5">
-              <input
-                type="file"
-                accept=".csv,.xls"
-                placeholder="Archivo CSV"
-                className="m-8 block"
-                onChange={(e) => setFile(e.target.files?.[0] || null)}
-              />
-              <div className="px-8 flex justify-center items-center">
-                <button
-                  onClick={handleSubmitFile}
-                  className="py-2.5 px-10 mr-2 mb-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-pink-700 focus:z-10 focus:ring-4 focus:ring-pink-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-pink-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-pink-700"
-                >
-                  Procesar archivo
-                </button>
+            {fileUploading ? (
+              <div className="w-[60%] h-full justify-center content-center flex flex-row">
+                <div className="">
+                  <Lottie animationData={cat} />
+                </div>
               </div>
+            ) : (
+              <div className="rounded-md border-dashed border-pink-500 border-2 p-2 m-5">
+                <input
+                  type="file"
+                  accept=".csv,.xls,.xlsx"
+                  placeholder="Archivo CSV"
+                  className="m-8 block"
+                  onChange={(e) => setFile(e.target.files?.[0] || null)}
+                />
+                <div className="px-8 flex justify-center items-center">
+                  <button
+                    onClick={handleSubmitFile}
+                    className="py-2.5 px-10 mr-2 mb-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-pink-700 focus:z-10 focus:ring-4 focus:ring-pink-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-pink-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-pink-700"
+                  >
+                    Procesar archivo
+                  </button>
+                </div>
 
-              <div className="px-10 text-center">
-                {fileResponse
-                  ? fileResponse.message + " lineas : " + fileResponse.lines
-                  : "No se ha procesado "}
+                <div className="px-10 text-center">
+                  {fileResponse
+                    ? fileResponse.message + " lineas : " + fileResponse.lines
+                    : "No se ha procesado "}
+                </div>
               </div>
-            </div>
+            )}
           </div>
           <div className=" bg-pink-50 shadow-xl rounded-md p-10 m-5">
             <h2 className="text-2xl font-medium text-pink-900">
@@ -81,16 +119,12 @@ export default function Archivo() {
               el resultado de tu predicción
             </h1>
             <div className=" rounded-md border-dashed border-pink-500 border-2 p-2 m-5">
-              <h3 className="py-5 px-3">Excel con predicción</h3>
-              <Image
-                className="max-w-full  "
-                src="/images/excel.png"
-                alt="Logo tec "
-                width={50}
-                height={50}
-              />
+              <h3 className="py-5 px-3">Nuevo excel</h3>
               <div className="px-8 flex justify-center items-center">
-                <button className=" py-2.5 px-10 mr-2 mb-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-pink-700 focus:z-10 focus:ring-4 focus:ring-pink-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-pink-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-pink-700">
+                <button
+                  onClick={downloadFile}
+                  className=" py-2.5 px-10 mr-2 mb-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-pink-700 focus:z-10 focus:ring-4 focus:ring-pink-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-pink-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-pink-700"
+                >
                   Descargar
                 </button>
               </div>
